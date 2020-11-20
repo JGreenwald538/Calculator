c17c2baaec45be6e463df988f2e4a4ac159b2b55
JGreenwald538 <72763093+JGreenwald538@users.noreply.github.com>
2020-11-20 09:27:35 -0500

    Update Calculator.py
    Added Log2, sin, cos, tan.

--- Calculator.py
+++ Calculator.py
@@ -90,8 +90,16 @@ elif op == "log":
         num1 = math.e
     if num1 == "pi":
         num1 = math.pi
-    num2 = math.log(float(num1))
-    print("e to the " + str(num2) + " power gets you " + num1)
+    num3 = math.log(float(num1))
+    print("e to the " + str(num3) + " power gets you " + str(num1))
+elif op == "log2":
+    num1 = (input("Enter the constant number:"))
+    if num1 == "e":
+        num1 = math.e
+    if num1 == "pi":
+        num1 = math.pi
+    num2 = math.log2(float(num1))
+    print("2 to the " + str(num2) + " power gets you " + num1)
 elif op == "log10":
     num1 = (input("Enter the constant number:"))
     if num1 == "e":
@@ -100,6 +108,33 @@ elif op == "log10":
         num1 = math.pi
     num2 = math.log10(float(num1))
     print("10 to the " + str(num2) + " power gets you " + num1)
+elif op == "sin":
+    num1 = (input("Enter a number:"))
+    if num1 == "e":
+        num1 = math.e
+    if num1 == "pi":
+        num1 = math.pi
+    num2 = math.sin(float(num1))
+    num3 = math.degrees(num2)
+    print("The sine of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
+elif op == "cos":
+    num1 = (input("Enter a number:"))
+    if num1 == "e":
+        num1 = math.e
+    if num1 == "pi":
+        num1 = math.pi
+    num2 = math.cos(float(num1))
+    num3 = math.degrees(num2)
+    print("The cosine of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
+elif op == "tan":
+    num1 = (input("Enter a number:"))
+    if num1 == "e":
+        num1 = math.e
+    if num1 == "pi":
+        num1 = math.pi
+    num2 = math.tan(float(num1))
+    num3 = math.degrees(num2)
+    print("The tangent of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
 else:
     print("Invalid Operator")
 
@@ -171,7 +206,7 @@ while repeat == "Yes":
             num1 = math.pi
         num3 = math.pow(float(num1), float(num2))
         print(str(num1) + "^" + str(num2) + " = " + str(num3))
-    elif op == "Sqrt":
+    elif op == "sqrt":
         num1 = (input("Enter a number:"))
         if num1 == "e":
             num1 = math.e
@@ -179,7 +214,7 @@ while repeat == "Yes":
             num1 = math.pi
         num2 = math.sqrt(float(num1))
         print("The square root of " + str(num1) + " is " + str(num2))
-    elif op == "Exp":
+    elif op == "exp":
         num1 = (input("Enter a number:"))
         if num1 == "e":
             num1 = math.e
@@ -187,14 +222,22 @@ while repeat == "Yes":
             num1 = math.pi
         num2 = math.exp(float(num1))
         print("e to the " + str(num1) + " power is " + str(num2))
-    elif op == "Log":
+    elif op == "log":
         num1 = (input("Enter the constant number:"))
         if num1 == "e":
             num1 = math.e
         if num1 == "pi":
             num1 = math.pi
         num2 = math.log(float(num1))
-    elif op == "Log10":
+    elif op == "log2":
+        num1 = (input("Enter the constant number:"))
+        if num1 == "e":
+            num1 = math.e
+        if num1 == "pi":
+            num1 = math.pi
+        num2 = math.log2(float(num1))
+        print("2 to the " + str(num2) + " power gets you " + num1)
+    elif op == "log10":
         num1 = (input("Enter the constant number:"))
         if num1 == "e":
             num1 = math.e
@@ -202,6 +245,33 @@ while repeat == "Yes":
             num1 = math.pi
         num2 = math.log10(float(num1))
         print("10 to the " + str(num2) + " power gets you " + num1)
+    elif op == "sin":
+        num1 = (input("Enter a number:"))
+        if num1 == "e":
+            num1 = math.e
+        if num1 == "pi":
+            num1 = math.pi
+        num2 = math.sin(float(num1))
+        num3 = math.degrees(num2)
+        print("The sine of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
+    elif op == "cos":
+        num1 = (input("Enter a number:"))
+        if num1 == "e":
+            num1 = math.e
+        if num1 == "pi":
+            num1 = math.pi
+        num2 = math.cos(float(num1))
+        num3 = math.degrees(num2)
+        print("The cosine of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
+    elif op == "tan":
+        num1 = (input("Enter a number:"))
+        if num1 == "e":
+            num1 = math.e
+        if num1 == "pi":
+            num1 = math.pi
+        num2 = math.tan(float(num1))
+        num3 = math.degrees(num2)
+        print("The tangent of " + num1 + " is " + str(num2) + " in radians and " + str(num3) + " degrees")
     else:
         print("Invalid Operator")
     repeat = input("Would you like to repeat?:")
